5a042887f290beecf66ad31e353075b4d9fc0b15
chan <aszwt12@gmail.com>
2023-09-12 09:45:51 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-KDT-AI-2/ReviewRepo.git

---
+++ src/main/java/com/smhrd/model/Cart.java
@@ -0,0 +1,16 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+	
+	private String user_id;
+	private String pro_id;
+	private int pro_volume;
+}
---
+++ src/main/java/com/smhrd/model/ProductInfo.java
@@ -0,0 +1,16 @@
+package com.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductInfo {
+	private String pro_id;
+	private String pro_name;
+	private int pro_price;
+	private int pro_volume;
+	
+}
